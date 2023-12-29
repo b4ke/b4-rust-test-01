86fec97e189d04dfb1208c186e7ec532ed043bf6
mic baker <op@mic.quest>
2023-12-29 03:45:49 +0000

    pair effort 01

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 //auth: mic
-//time: 2021-07-06 22:00:00
-fn main() {
-    println!("Hello, world!");
+//ver: 0.0.1
+//time: 2023-12-28 22:00:00;
+use axum::{
+    routing::get,
+    Router,
+};
+use hyper::Server;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
